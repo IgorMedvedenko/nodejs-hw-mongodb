8a23f3a46673702dee498b5d0e24e7e07fc33969
IgorMedvedenko <noveciti0@gmail.com>
2025-05-13 22:57:57 +0300

    fix1

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -11,7 +11,7 @@ const contactSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    isFavorite: {
+    isFavourite: {
       type: Boolean,
       default: false,
     },
