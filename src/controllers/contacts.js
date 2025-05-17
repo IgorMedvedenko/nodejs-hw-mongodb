d2b38fb1ea2a6cdc1c143da052c673642249ad88
IgorMedvedenko <noveciti0@gmail.com>
2025-05-17 23:41:39 +0300

    fix5

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,7 +47,7 @@ export const patchContactController = async (req, res, next) => {
   try {
     const result = await updateContact(contactId, req.body);
     if (!result) {
-      next(createHttpError(404, 'Contact not found'));
+      throw createHttpError(404, 'Contact not found');
     }
     res.status(200).json({
       status: 200,
