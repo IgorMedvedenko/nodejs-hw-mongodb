83e3fd3efead8fe775f99971e07934350795e27e
IgorMedvedenko <noveciti0@gmail.com>
2025-05-20 22:35:07 +0300

    fix15

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -10,7 +10,7 @@ export function errorHandler(err, req, res, next) {
 
   res.status(500).json({
     status: 500,
-    message: 'Something went wrong',
+    message: 'Something wenttt wrong',
   });
   next();
 }
