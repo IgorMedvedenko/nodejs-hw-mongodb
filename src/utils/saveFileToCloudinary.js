c2278fa77075ec4e79a1d6de4d7863ba555c5e68
IgorMedvedenko <noveciti0@gmail.com>
2025-06-15 13:02:49 +0300

    try1

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,5 +1,4 @@
 import cloudinary from 'cloudinary';
-import fs from 'node:fs/promises';
 import { getEnvVar } from './getEnvVar.js';
 import { CLOUDINARY } from '../constants/index.js';
 
@@ -10,7 +9,13 @@ cloudinary.v2.config({
   api_secret: getEnvVar(CLOUDINARY.API_SECRET),
 });
 export const saveFileToCloudinary = async (file) => {
-  const response = await cloudinary.v2.uploader.upload(file.path);
-  await fs.unlink(file.path);
+  const response = await new Promise((resolve, reject) => {
+    cloudinary.v2.uploader
+      .upload_stream({ resource_type: 'auto' }, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      })
+      .end(file.buffer);
+  });
   return response.secure_url;
 };
